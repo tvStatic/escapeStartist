cf324da7a95537ecf0060c982c800acf01bda22f
tvStatic <757946+tvStatic@users.noreply.github.com>
2020-04-06 13:19:04 +1000

    Add menu animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state("open", style({
+        opacity: 1
+      })),
+      state("closed", style({
+        opacity: 0
+      })),
+      transition("open => closed", [
+        animate("0.5s")
+      ]),
+      transition("closed => open", [
+        animate("0.5s")
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'escapeHome';
